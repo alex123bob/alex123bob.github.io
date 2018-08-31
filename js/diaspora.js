2abd11cd20f66c08c5c75bc70bec4806b661ab8d
Li <jiali@mac-jiali1.corp.microstrategy.com>
2018-08-31 10:27:38 -0400

    Site updated: 2018-08-31 10:27:37

--- js/diaspora.js
+++ js/diaspora.js
@@ -466,13 +466,12 @@ $(function() {
             case - 1 != tag.indexOf("comment"): 
                 Diaspora.loading(),
                 comment = $('#gitalk-container');
-                debugger
                 gitalk = new Gitalk({
                   clientID: comment.data('ci') || 'a9acae2fbd0522db8c41',
                   clientSecret: comment.data('cs') || '8abe1956860f894074f6223626cd570aa273d04a',
-                  repo: comment.data('r') || 'https://alex123bob.github.io',
-                  owner: comment.data('o'),
-                  admin: comment.data('a'),
+                  repo: comment.data('r') || 'alex123bob.github.io',
+                  owner: comment.data('o') || 'alex123bob.github.io',
+                  admin: comment.data('a') || 'alex123bob.github.io',
                   id: location.pathname,
                   distractionFreeMode: comment.data('d')
                 })
